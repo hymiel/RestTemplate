b4ca0ac81d9fdd219a698564a3a51238a8abdeee
Lee hyunkyoung <hyunk.office@gmail.com>
2023-06-20 18:35:46 +0900

    postForEntity

--- src/main/java/com/sparta/springresttemplateserver/controller/ItemController.java
+++ src/main/java/com/sparta/springresttemplateserver/controller/ItemController.java
@@ -3,7 +3,7 @@ package com.sparta.springresttemplateserver.controller;
 import com.sparta.springresttemplateserver.dto.ItemResponseDto;
 import com.sparta.springresttemplateserver.dto.UserRequestDto;
 import com.sparta.springresttemplateserver.entity.Item;
-import com.sparta.springresttemplateserver.dto.ItemService;
+import com.sparta.springresttemplateserver.service.ItemService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
